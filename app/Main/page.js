73db327dc1d67d28a87b85b093ff6ecb5fbcedfc
yashgoel <yash.goel8370@gmail.com>
2025-05-11 21:05:47 +0530

    commit

--- app/Main/page.js
+++ app/Main/page.js
@@ -3,11 +3,11 @@
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 // import { auth, provider as GoogleProvider, signInWithPopup } from "../firebase";
+import useDarkMode from "../darkMode";
 
 import Image from "next/image";
 import logo from "../../public/WalboLogo.png";
 import darkModeLogo from "../../public/WalboLogoDarkMode.png";
-import metamask from "../../public/MetaMask.png";
 import metamaskbg from "../../public/matamask-withoutbg.png";
 // import google from "../../public/Google.png";
 import "./page.css";
@@ -16,6 +16,8 @@ import { createWalletClient, custom } from "viem";
 import { flare, sepolia } from "viem/chains";
 
 function Main() {
+  const [isDark, toggleDarkMode] = useDarkMode();
+
   const router = useRouter();
   const [metamaskinstalled, setmetamaskinstalled] = useState(true);
   const [isConnecting, setisConnecting] = useState(false);
@@ -76,10 +78,32 @@ function Main() {
 
   return (
     <>
+      <div className="darkModeToggle">{isDark ? (
+        <svg onClick={toggleDarkMode}
+          xmlns="http://www.w3.org/2000/svg"
+          height="30px"
+          viewBox="0 -960 960 960"
+          width="30px"
+          fill="#ffffff"
+        >
+          <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+        </svg>
+      ) : (
+        <svg onClick={toggleDarkMode}
+          xmlns="http://www.w3.org/2000/svg"
+          height="30px"
+          viewBox="0 -960 960 960"
+          width="30px"
+          fill="#000000"
+        >
+          <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
+        </svg>
+      )}</div>
+      
       <div className={`walbo-container`}>
         <div className="main-container">
           <div className="flex flex-col">
-            <Image src={logo} width={350} alt="Walbo" priority></Image>
+            {isDark ? <Image src={darkModeLogo} width={350} alt="Walbo"></Image> : <Image src={logo} width={350} alt="Walbo" priority></Image>}
 
             <div className="login">
               <div className="SocialLoginContainer">
@@ -95,7 +119,7 @@ function Main() {
                 {signInWithMetaMask ? (
                   <div className="SocialLogin" onClick={handleMetaMaskLogin}>
                     <div className="Image">
-                      <Image src={metamask} width={35} alt="MetaMask"></Image>
+                      <Image src={metamaskbg} width={35} alt="MetaMask"></Image>
                     </div>
                     <div className={`content`}>Continue with MetaMask</div>
                   </div>
--- app/TransactionHistory/page.js
+++ app/TransactionHistory/page.js
@@ -1,5 +1,6 @@
 "use client";
-
+import useDarkMode from "../darkMode";
+import darkModeLogo from "../../public/WalboLogoDarkMode.png";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { createWalletClient, custom } from "viem";
@@ -14,6 +15,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
 function ContactList() {
+  const [isDark, toggleDarkMode] = useDarkMode();
   const router = useRouter();
   const [address, setAddress] = useState("");
   const [transactionHistory, setTransactionHistory] = useState([]);
@@ -114,10 +116,30 @@ function ContactList() {
 
   return (
     <>
-      <div className="DashboardHeader">
+      <div className="darkModeToggle">{isDark ? (
+        <svg onClick={toggleDarkMode}
+          xmlns="http://www.w3.org/2000/svg"
+          height="30px"
+          viewBox="0 -960 960 960"
+          width="30px"
+          fill="#ffffff"
+        >
+          <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+        </svg>
+      ) : (
+        <svg onClick={toggleDarkMode}
+          xmlns="http://www.w3.org/2000/svg"
+          height="30px"
+          viewBox="0 -960 960 960"
+          width="30px"
+          fill="#000000"
+        >
+          <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
+        </svg>
+      )}</div>
+      <div className={`DashboardHeader${isDark ? "-dark" : ""}`}>
         <div className="logo">
-          <Image src={logo} alt="Walbo" priority />
-        </div>
+{isDark ? <Image src={darkModeLogo} width={350} alt="Walbo"></Image> : <Image src={logo} width={350} alt="Walbo" priority></Image>}        </div>
         <button
           className="hamburger"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -166,13 +188,13 @@ function ContactList() {
         {info ? (
           <div className="transactionInfo">
             <button
-              className="btn btn-secondary mb-3"
+              className="btn btn-secondary mb-2"
               onClick={handleBackButton}
             >
               Back to Transactions
             </button>
             <h4>Transaction Details</h4>
-            <div className="card">
+            <div className={`card mb-3 ${isDark ? "bg-dark text-white" : ""}`}>
               <div className="card-body">
                 <div className="d-flex justify-content-between align-items-center px-5 mb-4">
                   <div
@@ -189,7 +211,15 @@ function ContactList() {
                   </div>
 
                   <div className="text-center" style={{ flex: "0 0 auto" }}>
-                    <svg
+                    {isDark ? <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="30px"
+                      viewBox="0 -960 960 960"
+                      width="30px"
+                      fill="#ffffff"
+                    >
+                      <path d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                    </svg> : <svg
                       xmlns="http://www.w3.org/2000/svg"
                       height="30px"
                       viewBox="0 -960 960 960"
@@ -197,7 +227,7 @@ function ContactList() {
                       fill="#000000"
                     >
                       <path d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-                    </svg>
+                    </svg>}
                   </div>
 
                   <div
@@ -260,7 +290,7 @@ function ContactList() {
           </div>
               ) : transactionHistory.length > 0 ? (
                       <div className="table-responsive">
-          <table className="table table-hover container text-end">
+          <table className={`table table-hover container text-end ${isDark ? "table-dark" : ""}`}>
             <thead>
               <tr>
                 <th scope="col">Type</th>
@@ -268,7 +298,8 @@ function ContactList() {
                 <th scope="col">Time</th>
                 <th scope="col">From</th>
                 <th scope="col">To</th>
-                <th scope="col">Amount</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Remarks</th>
                 <th scope="col">Status</th>
               </tr>
             </thead>
@@ -315,6 +346,7 @@ function ContactList() {
                       transaction.toPublicKey}
                   </td>
                   <td>{transaction.amount} SepoliaETH</td>
+                  <td>{transaction.remark || "N/A"}</td>
                   <td>
                     {transaction.status === "Pending" ? (
                       <span className="badge bg-warning">Pending</span>
